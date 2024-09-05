d00359b7888834b4ba832b80b9cd5b8827ce6a25
Antony _Gusmão <88747866+AntonyGuzma@users.noreply.github.com>
2024-09-05 10:59:53 -0400

    :construction: Adicionanco o modelo multimodal que analisa imagens

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 import { fazerPergunta } from './pergunta.js'
 import { perguntar } from './perguntaLivre.js'
 import { consultar } from './consultaDestino.js'
+import { processaImagem } from './processaImagem.js'
 
 async function principal() {
   const escolha = await fazerPergunta(`Escolha uma das opções abaixo: \n
   1. Fazer uma pergunta livre sobre um destino;
   2. Comparação de destinos por categorias;
+  3. Ver informação com base em imagem;
   \nOpção desejada: `)
 
   if (escolha === '1') {
     await perguntar()
   } else if (escolha === '2') {
     await consultar()
+  } else if (escolha === '3'){
+    const imagem = await fazerPergunta('Me informe o path da imagem: ')
+    await processaImagem(imagem)
   } else {
     console.log('Escolha inválida.')
   }
---
+++ processaImagem.js
@@ -0,0 +1,27 @@
+import { inicializaModelo } from './modelo.js'
+import { readFileSync } from "fs"
+import { fazerPergunta } from './pergunta.js'
+const model = await inicializaModelo("gemini-1.5-pro")
+
+// Converts local file information to a GoogleGenerativeAI.Part object.
+function fileToGenerativePart(path, mimeType) {
+    return {
+      inlineData: {
+        data: Buffer.from(readFileSync(path)).toString("base64"),
+        mimeType
+      },
+    }
+  }
+
+export async function processaImagem(imagem) {
+    const prompt = await fazerPergunta('Pergunte sobre a imagem: ')
+
+    const imageParts =[
+        fileToGenerativePart(imagem, "image/jpg")
+    ]
+
+    const result = await model.generateContent([prompt, ...imageParts]);
+    const response = await result.response
+    const text = response.text()
+    console.log(text)
+}
