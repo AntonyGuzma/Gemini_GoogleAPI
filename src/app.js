4bb346b6ec19a951cdda3779a6ed7cfd7075e8b8
Antony _Gusmão <88747866+AntonyGuzma@users.noreply.github.com>
2024-09-09 15:45:04 -0400

    :sparkles: feat adicionando codigo base para o chatbot

--- src/app.js
+++ src/app.js
@@ -1,29 +1,20 @@
-import { fazerPergunta } from '../src/services/pergunta.js'
-import { perguntar } from '../src/services/perguntaLivre.js'
-import { consultar } from '../src/services/consultaDestino.js'
-import { processaImagem } from '../src/services/processaImagem.js'
-import { processaArquivoTexto } from '../src/services/categorizador.js'
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 
-async function principal() {
-  const escolha = await fazerPergunta(`Escolha uma das opções abaixo: \n
-  1. Fazer uma pergunta livre sobre um destino;
-  2. Comparação de destinos por categorias;
-  3. Ver informação com base em imagem;
-  4. Fazer a análise de sentimentos com base em arquivo texto;
-  \nOpção desejada: `)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-  if (escolha === '1') {
-    await perguntar()
-  } else if (escolha === '2') {
-    await consultar()
-  } else if (escolha === '3'){
-    const imagem = await fazerPergunta('Me informe o path da imagem com prefixo: test/ + path: ')
-    await processaImagem(imagem)
-  } else if (escolha === '4'){
-    await processaArquivoTexto()
-  } else {
-    console.log('Escolha inválida.')
-  }
-}
+const app = express();
+const port = process.env.PORT || 3000;
 
-principal()
+app.use(express.json());
+app.use('/static', express.static(join(__dirname, 'static'), { extensions: ['css', 'svg', 'js'] }));
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'templates', 'chat.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
